b752266bb7f94ac1cc3a438c2f1814fed3461a21
Vyacheslav <vyacheslav.selyakov@mail.ru>
2019-03-06 13:35:27 +0300

    Add method what loaded databases on input server. Loading of databases start when combobox of databases is opened

--- WpfSqlConnectionLibrary/Windows/SqlConnectionWindow.xaml.cs
+++ WpfSqlConnectionLibrary/Windows/SqlConnectionWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -14,6 +15,7 @@ namespace WpfSqlConnectionLibrary.Windows
     {
         private Visibility _visibleLoadingRing = Visibility.Visible;
         private List<string> _serverList;
+        private List<string> _databaseList;
 
         /// <summary>
         /// Строка подключения к серверу.
@@ -38,14 +40,30 @@ namespace WpfSqlConnectionLibrary.Windows
             set { _serverList = value; OnPropertyChanged("ServerList"); }
         }
 
+        /// <summary>
+        /// Список доступных баз данных.
+        /// </summary>
+        public List<string> DatabaseList
+        {
+            get { return _databaseList; }
+            set { _databaseList = value; OnPropertyChanged("DatabaseList"); }
+        }
+
         public SqlConnectionWindow()
         {
             InitializeComponent();
 
             ServerList = new List<string>();
-            LoadServers();
+            DatabaseList = new List<string>();
+
+            LoadServersAsync();
 
             DataContext = this;
+        }
+
+        private void ComboDatabaseName_DropDownOpened(object sender, EventArgs e)
+        {
+            LoadDatabases();            
         }
 
         private void BtnApply_Click(object sender, RoutedEventArgs e)
@@ -68,16 +86,32 @@ namespace WpfSqlConnectionLibrary.Windows
             this.DialogResult = true;
         }
 
+        private void LoadServers()
+        {
+            VisibleLoadingRing = Visibility.Visible;
+
+            DataTable dt = System.Data.Sql.SqlDataSourceEnumerator.Instance.GetDataSources();
+            comboServerName.Items.Clear();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                comboServerName.Items.Add(string.Concat(dr["ServerName"], "\\", dr["InstanceName"]));
+            }
+
+            VisibleLoadingRing = Visibility.Hidden;
+        }
+
         /// <summary>
         /// Загрузить список доступных серверов.
         /// </summary>
-        private void LoadServers()
+        private void LoadServersAsync()
         {
             Task.Factory.StartNew(() =>
             {
                 VisibleLoadingRing = Visibility.Visible;
 
                 DataTable dt = System.Data.Sql.SqlDataSourceEnumerator.Instance.GetDataSources();
+                ServerList.Clear();
 
                 foreach (DataRow dr in dt.Rows)
                 {
@@ -93,28 +127,71 @@ namespace WpfSqlConnectionLibrary.Windows
         /// </summary>
         private void LoadDatabases()
         {
-            //string connectionString = Connections.MainConnetction.GetSQLConnection;
-            //string request = String.Format("EXEC sp_databases");
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    try
-            //    {
-            //        connection.Open();
-            //        SqlDataAdapter adapter = new SqlDataAdapter(request, connection);
-            //        DataTable ds = new DataTable();
-            //        adapter.Fill(ds);
+            comboDatabaseName.Items.Clear();
 
-            //        foreach (DataRow dr in ds.Rows)
-            //        {
-            //            comboDatabases.Items.Add(string.Concat(dr["DATABASE_NAME"]));
-            //        }
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        comboServers.Items.Clear();
-            //        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    }
-            //}
+            string connectionString = String.Format(@"Data Source={0};Initial Catalog=master;Integrated Security=True",
+                comboServerName.Text);
+
+            string request = String.Format("EXEC sp_databases");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(request, connection);
+                    DataTable ds = new DataTable();
+                    adapter.Fill(ds);
+
+                    foreach (DataRow dr in ds.Rows)
+                    {
+                        comboDatabaseName.Items.Add(string.Concat(dr["DATABASE_NAME"]));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    comboDatabaseName.Items.Clear();
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void LoadDataBasesAsync()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                //VisibleLoadingRing = Visibility.Visible;
+
+                DatabaseList.Clear();
+                string server = String.Empty;
+                comboServerName.Dispatcher.Invoke(new Action(() => server = comboServerName.Text));
+
+                string connectionString = String.Format(@"Data Source={0};Initial Catalog=master;Integrated Security=True",
+                    server);
+                string request = String.Format("EXEC sp_databases");
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+                        SqlDataAdapter adapter = new SqlDataAdapter(request, connection);
+                        DataTable ds = new DataTable();
+                        adapter.Fill(ds);
+
+                        foreach (DataRow dr in ds.Rows)
+                        {
+                            DatabaseList.Add(string.Concat(dr["DATABASE_NAME"]));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        DatabaseList.Clear();
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+
+                //VisibleLoadingRing = Visibility.Hidden;
+            });
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
